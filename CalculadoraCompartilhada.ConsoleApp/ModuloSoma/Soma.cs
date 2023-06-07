5682a5d8a5fa214844f8f8c249f961f7358a6d7f
Slaviero <slaviero.kaua@gmail.com>
2023-06-07 15:08:55 -0300

    Create Feature de soma
    criei a função somar() com parametro de 2 números inteiros e retorno de uma string com o resultado.

---
+++ CalculadoraCompartilhada.ConsoleApp/ModuloSoma/Soma.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraCompartilhada.ConsoleApp.ModuloSoma
+{
+    public class Soma
+    {
+
+       public string somar(int primeiroValor, int segundoValor)
+       {
+            int resultado = primeiroValor + segundoValor;
+
+            return $"O valor de soma entre {primeiroValor} mais {primeiroValor} é : {resultado}";
+       }
+    }
+}
