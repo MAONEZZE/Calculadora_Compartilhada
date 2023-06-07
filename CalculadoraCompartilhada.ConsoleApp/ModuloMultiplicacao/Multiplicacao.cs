45b20afcf80e7aea5ed18b243314320301ad46bd
FranciscoPaixao <26686029+FranciscoPaixao@users.noreply.github.com>
2023-06-07 14:48:49 -0300

    Create Multiplicacao.cs

---
+++ CalculadoraCompartilhada.ConsoleApp/ModuloMultiplicacao/Multiplicacao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraCompartilhada.ConsoleApp.ModuloMultiplicacao
+{
+    public class Multiplicacao
+    {
+        public Multiplicacao(decimal primeiroNumero, decimal segundoNumero)
+        {
+            
+        }
+    }
+}
